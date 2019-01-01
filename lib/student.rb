e21fe3c4a06ff196175a2d9ca11709c679584c87
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-01 18:24:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 9
+      WHERE grade = 10
       LIMIT ? ;
     SQL
     table = DB[:conn].execute(sql, number)
